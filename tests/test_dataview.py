f915f79f568e25021ca7c6b694d24ea6fec7f369
Christopher Brown <code-breaker@code-breaker-desktop.(none)>
2010-07-03 11:58:49 -0700

    New dataview test

--- tests/test_dataview.py
+++ tests/test_dataview.py
@@ -2,17 +2,21 @@
 import sys, os
 sys.path.append(os.path.join("..","src"))
 import pal
+import numpy as np
 
 if __name__ == "__main__":
-    d = pal.dataview.from_csv("data/three.csv", dv="dv")
-    v = d.view({'a':[],'b':[]})
-    #d = Dataset("data/threebad.csv", dv="dv", subject=None)
 
-    print v.treatments
+    ## View demo:
+
+    # Read all data
+    d = pal.dataview.from_csv("data/three.csv", dv="dv")
+    # Create a view of the data where a = a1, and b = b1 | b2
+    # This will give us a mean property with 2 values,
+    # one for a1b1, one for a1b2
+    v = d.view({'a':['a1'],'b':['b1','b2']})
+    # The corresponding data pulled manually from the data file
+    a1b1 = np.array((4.1, 4.3, 4.5, 3.8, 4.3, 4.8, 4.5, 5.0, 4.6, 5.0))
+    a1b2 = np.array((4.6, 4.9, 4.2, 4.5, 4.8, 5.6, 5.8, 5.4, 6.1, 5.4))
+
+    print np.mean(a1b1), np.mean(a1b2)
     print v.mean
-    #antb = fact_anova(d.rawdata, d.ivs)
-    #antb = fact_anova(d.rawdata)
-    # pretty print
-    #for x in antb:
-    #    a,b,c,d,e,f = x
-    #    print "%s\t%.3f\t%d\t%.3f\t%.3f\t%.3f" % (a,b,c,d,e,f)
