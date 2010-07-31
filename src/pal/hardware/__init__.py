dcedacb000208218a152dd1f02bce452bf4d984c
Christopher Brown <code-breaker@code-breaker-desktop.(none)>
2010-07-30 22:20:45 -0700

    Commit working pa4 module, since gitorious was having problems earlier. Created hardware module. That and misc are not imported by default.

--- src/pal/hardware/__init__.py
+++ src/pal/hardware/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2010 Christopher Brown and Joseph Ranweiler;
-# All Rights Reserved.
+# Copyright (c) 2010 Christopher Brown; All Rights Reserved.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -31,16 +30,42 @@
 # Comments and/or additions are welcome (send e-mail to: c-b@asu.edu).
 #
 
-'''
-PAL - Psychoacoustics Lab
+audiere_present = True
+pa4_present = True
 
-'''
+try:
+    import pa4
+except ImportError, e:
+    print "Could not import pa4"
+    pa4_present = False
 
-__version__ = '0.2'
+try:
+    import audiere
+except ImportError, e:
+    print "Could not import pyaudiere"
+    audiere_present = False
 
-from nan import nanproduct,nanmean
-from array import array
-from dataview import dataview
-#from misc import csv_inspect, formatplot
-import signal
-import stats
+if audiere_present:
+    audiere_device = audiere.open_device()
+    def wavplay(buff, fs, pan=0):
+        '''Pass a sound buffer to the soundcard
+
+            Plays a numpy array with blocking, matlab-style
+
+            Parameters
+            ----------
+            buff : array
+                a sound buffer to play.
+            fs : scalar
+                The sampling frequency.
+            pan : scalar
+                  1 = pan all the way to right
+                -1 = pan all the way to left
+                  0 = no pan [default]
+          '''
+        s = audiere_device.open_array(buff,fs)
+        s.pan = pan
+        s.play()
+        while s.playing:
+                sleep(.01)
+        del(s,d)
---
+++ src/pal/hardware/pa4.py
@@ -0,0 +1,205 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2010 Christopher Brown; All Rights Reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in
+#      the documentation and/or other materials provided with the distribution
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# Comments and/or additions are welcome (send e-mail to: c-b@asu.edu).
+#
+
+"""
+    pa4 - functions to control TDT pa4 attenuators using the serial port
+
+    functions include:
+
+    set_atten(device, atten, com='COM1')
+        Sets the specified attenuation level on the specified device.
+        Device 1 is the first module in the first xbus, etc. Atten is
+        a float in dB, between 0 <= 99.9. com is the serial port used.
+
+    get_atten(device, com='COM1')
+        Gets the current attenuation level on the specified device.
+
+    set_mute(device, mute=False, com='COM1')
+        Sets the mute for the specified device. mute is a bool.
+
+    find_pa4(com='COM1')
+        Scans the first 32 device IDs, looking for PA4s.
+
+    To access pa4's via the serial port, set both jumpers on the back
+    of the OI1 to the RIGHT.
+
+    These functions are based on C code from the House Ear Institute,
+    provided by John Wygonski.
+
+    Tested on windows and linux.
+
+    depends on pyserial
+"""
+
+import serial
+
+commands = { 'MUTE':   0x15,
+             'NOMUTE': 0x16,
+             'READ':   0x18,
+             'ATTEN':  0x20,
+           }
+
+success = '\xc3'
+
+def set_atten(dev, atten, port='COM1'):
+    '''Sets the specified attenuation level on the specified device.
+
+        Parameters
+        ----------
+        dev : int
+            The device ID. Device 1 is the first module in the first
+            xbus, etc.
+        atten : int
+            The amount of attenuation, in dB. Should be 0 <= 99.9
+
+        port : str
+            The serial port to use. Default is 'COM1'. On my linux
+            box, it is '/dev/ttyS0'
+
+        Returns
+        -------
+        None.
+    '''
+    d = chr(3+dev)         # device ID's start at 4
+    b = chr(0x40 + 4)      # Number of bytes to follow (including checksum)
+    ch = commands['ATTEN'] # command for set atten
+    c = chr(ch)
+    lo,hi = lohibytes(atten * 10)
+    cs,j = lohibytes(ch+lo+hi)
+    command = d + b + c + chr(hi) + chr(lo) + chr(cs)
+
+    s = serial.Serial(port, baudrate=38400, timeout=1)
+    s.write(command)
+    ret = s.readline()
+    s.close()
+    if ret != success:
+        raise Exception, 'Error accessing hardware'
+
+def find_pa4(port='COM1'):
+    '''Scans the first 32 device IDs, looking for PA4s.
+
+        Parameters
+        ----------
+        port : str
+            The serial port to use. Default is 'COM1'. On my linux
+            box, it is '/dev/ttyS0'
+
+        Returns
+        -------
+        devs : list
+            A list of PA4 device IDs
+
+    '''
+    devlist = []
+    ch = commands['READ']
+    c = chr(ch)
+    s = serial.Serial(port, baudrate=38400, timeout=.1)
+    for dev in range(1,33):
+        s.write(chr(3+dev) + c)
+        ret = s.readline()
+        if ret != '' and ret[0] == success:
+            devlist.append(dev)
+
+    s.close()
+    return devlist
+
+
+def get_atten(dev, port='COM1'):
+    '''Gets the current attenuation level on the specified device.
+
+        Parameters
+        ----------
+        dev : int
+            The device ID. Device 1 is the first module in the first
+            xbus, etc.
+        port : str
+            The serial port to use. Default is 'COM1'. On my linux
+            box, it is '/dev/ttyS0'
+
+        Returns
+        -------
+        atten : float
+            The amount of attenuation, in dB. Will be 0 <= 99.9
+    '''
+    d = chr(3+dev)
+    ch = commands['READ']
+    c = chr(ch)
+    command = d + c
+
+    s = serial.Serial(port, baudrate=38400, timeout=1)
+    s.write(command)
+    ret = s.readline()
+    s.close()
+    if ret == '' or ret[0] != success:
+        raise Exception, 'Error accessing hardware'
+    return (ord(ret[2]) + ord(ret[1]) * 256) / 10.
+
+def set_mute(dev, mute=False, port='COM1'):
+    '''Sets the mute for the specified device. mute is a bool.
+
+        Parameters
+        ----------
+        dev : int
+            The device ID. Device 1 is the first module in the first
+            xbus, etc.
+        mute : bool
+            True to mute device, False to unmute it.
+
+        port : str
+            The serial port to use. Default is 'COM1'. On my linux
+            box, it is '/dev/ttyS0'
+
+        Returns
+        -------
+        None.
+    '''
+    d = chr(3+dev)
+    if mute:
+        ch = chr(commands['MUTE'])
+    else:
+        ch = chr(commands['NOMUTE'])
+    command = d + ch
+
+    s = serial.Serial(port, baudrate=38400, timeout=1)
+    s.write(command)
+    ret = s.readline()
+    s.close()
+    if ret != success:
+        raise Exception, 'Error accessing hardware'
+
+
+def lohibytes(val):
+    ha = hex(int(val))
+    bytes = ha[2:].zfill(4)
+    lo = int('0x%s' % bytes[2:4],16)
+    hi = int('0x%s' % bytes[0:2],16)
+    return lo,hi
