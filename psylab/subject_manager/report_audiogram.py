542fe124ab6d53cc746e3a70ca5c3efe030a34a9
Christopher Brown <code-breaker@shrsft163.shrs.pitt.edu>
2013-11-08 10:38:35 -0500

    report_audiogram improvements

--- psylab/subject_manager/report_audiogram.py
+++ psylab/subject_manager/report_audiogram.py
@@ -23,6 +23,7 @@
 # cbrown1@pitt.edu.
 #
 
+import os
 import sys
 import sqlite3
 from matplotlib import pyplot as pp
@@ -35,10 +36,31 @@ name = "Audiogram"
 
 def draw_page():
 
+    # Derive this from the column names
+    x = [125, 250, 500, 750, 1000, 1500, 2000, 3000, 4000, 6000, 8000]
+    
+    xpos = np.arange(1,len(x)+1)
+
     fh = pp.figure(figsize=(8.5, 11))
 
     # Set up a `page` axis, to place text etc.
     ap = fh.add_axes([0, 0, 1, 1])
+
+    af = fh.add_axes([.2, .3, .5*11/8.5, .5*8.5/11])
+    af.axhspan(-5,20, fc='0.7', ec='None', alpha=0.5)
+
+    af.set_ylim([-5, 115])
+    af.yaxis.set_ticks(range(0, 120, 10))
+    af.yaxis.set_ticklabels(range(0, 120, 10))
+    af.invert_yaxis()
+    af.set_xlim([0, 110])
+    af.xaxis.set_ticks(xpos)
+    af.xaxis.set_ticklabels(x)
+    af.set_xlim([.5, len(xpos)+.5])
+    af.grid(True)
+    af.set_ylabel('Threshold (Hearing Level)')
+    af.set_xlabel('Frequency (Hz)')
+
     ap.set_xticks([])
     ap.set_yticks([])
     for spine in ap.spines:
@@ -47,12 +69,23 @@ def draw_page():
     ap.set_ylim([0, 11])  # now in units of inches
     ap.invert_yaxis()
 
+    ap.text(4.25, 1, "Psychoacoustics Lab",horizontalalignment='center', fontsize="x-large")
+    ap.text(4.25, 1.3, "The University of Pittsburgh",horizontalalignment='center', fontsize="large")
+    ap.text(4.25, 1.75, "Hearing Screening",horizontalalignment='center', fontsize="large")
+
+    ap.text(6.4, 2.2, "Tester:",horizontalalignment='right')
+    ap.text(6.4, 2.5, "Date of Audiogram:",horizontalalignment='right')
+    ap.text(2, 2.2, "Patient Name:", horizontalalignment='right')
+    ap.text(2, 2.5, "Date of Birth:", horizontalalignment='right')
+    ap.text(2, 2.8, "Gender:", horizontalalignment='right')
+    ap.hlines([2.2, 2.5, 2.8], 2.1, 4.5)
+    ap.hlines([2.2, 2.5], 6.5, 7.5)
+
     ap.add_line(Line2D([1, 7.5],[3, 3],color='k',lw=2))
 
-    for i in np.arange(8.6,10,.35):
-	    ap.add_line(Line2D([5,7.5],[i,i],color='k',lw=1))
-	
-    ap.text(5, 8.6, "Notes:")
+    ap.text(5, 8.6, "Notes: For research purposes")
+    ap.text(5, 8.95, "only - not a clinical assessment")
+    ap.hlines(np.arange(8.6,10,.35), 5, 7.5)
 
     # Legend:
     lx = 1.4
@@ -73,7 +106,7 @@ def draw_page():
     ap.add_patch(mpp.FancyArrowPatch((lx+2.37,ly+1.13), (lx+2.27,ly+1.23), arrowstyle='->', lw=3, ec='r', mutation_scale=20))
     ap.add_patch(mpp.FancyArrowPatch((lx+2.97,ly+1.13), (lx+3.07,ly+1.23), arrowstyle='->', lw=3, ec='b', mutation_scale=20))
 
-    return fh,ap
+    return fh,ap,af
 
 
 def plot_data(ax, data, side):
@@ -110,24 +143,12 @@ def plot_data(ax, data, side):
             this_ind = xpos[i]
             ar = mpp.FancyArrowPatch((this_ind+mp_nr_x[0],this_data+mp_nr_y[0]), (this_ind+mp_nr_x[1],this_data+mp_nr_y[1]), arrowstyle='->', lw=3, ec=c, mutation_scale=20)
             ax.add_patch(ar)
-            
-    ax.set_ylim([-5, 115])
-    ax.yaxis.set_ticks(range(0, 120, 10))
-    ax.yaxis.set_ticklabels(range(0, 120, 10))
-    ax.invert_yaxis()
-    ax.set_xlim([0, 110])
-    ax.xaxis.set_ticks(xpos)
-    ax.xaxis.set_ticklabels(x)
-    ax.set_xlim([.5, len(xpos)+.5])
-    ax.grid(True)
-    ax.set_ylabel('Threshold (Hearing Level)')
-    ax.set_xlabel('Frequency (Hz)')
 
 def is_number(s):
     try:
         float(s)
         return True
-    except ValueError:
+    except ValueError, TypeError:
         return False
 
 def proc_subject(db, SubjN):
@@ -137,54 +158,59 @@ def proc_subject(db, SubjN):
     In this case, the full path to the db and the subject number will be 
     passed to the function.
     """
-    fh,ap = draw_page()
-
-    af = fh.add_axes([.2, .3, .5*11/8.5, .5*8.5/11])
-    af.axhspan(-5,20, fc='0.7', ec='None', alpha=0.5)
+    fh,ap,af = draw_page()
 
     #af.text(8,50,'V',horizontalalignment='center',verticalalignment='center', rotation=45,color='r',fontweight='bold', fontsize=14)
     #af.plot(8,50,marker='v',color='r')
-    print (db)
-    conn = sqlite3.connect(db)
-    c = conn.cursor()
-    c.execute("""SELECT FName,LName,DOB,Gender,User_Audio_Date,User_Audio_Tester FROM Subjects WHERE SubjN == \'%s\'""" % SubjN)
-    subject = c.fetchone()
-    if subject is not None:
+    x = [125, 250, 500, 750, 1000, 1500, 2000, 3000, 4000, 6000, 8000]
+    xpos = np.arange(1,len(x)+1)
 
-        ap.text(4.25, 1, "Psychoacoustics Lab",horizontalalignment='center', fontsize="x-large")
-        ap.text(7.5, 1.5, "Tester: %s" % subject[5],horizontalalignment='right')
-        ap.text(7.5, 1.7, "Date of Audiogram: %s" % subject[4],horizontalalignment='right')
+    if db and os.path.exists(db):
+        conn = sqlite3.connect(db)
+        c = conn.cursor()
+        c.execute("""SELECT FName,LName,DOB,Gender,User_Audio_Date,User_Audio_Tester FROM Subjects WHERE SubjN == \'%s\'""" % SubjN)
+        subject = list(c.fetchone())
+        for i in range(len(subject)):
+            if subject[i] is None:
+                subject[i] = ''
+        if subject is not None:
 
-        ap.text(1, 1.5, "Patient Name: %s %s" % (subject[0],subject[1]))
-        ap.text(1, 1.7, "Date of Birth: %s" % subject[2])
-        ap.text(1, 1.9, "Gender: %s" % subject[3])
+            ap.text(6.5, 2.2, "%s" % subject[5],horizontalalignment='left')
+            ap.text(6.5, 2.5, "%s" % subject[4],horizontalalignment='left')
+            ap.text(2.1, 2.2, "%s %s" % (subject[0],subject[1]), horizontalalignment='left')
+            ap.text(2.1, 2.5, "%s" % subject[2], horizontalalignment='left')
+            ap.text(2.1, 2.8, "%s" % subject[3], horizontalalignment='left')
 
-        c.execute("""SELECT User_L125,User_L250,User_L500,User_L750,User_L1k,
-                     User_L15,User_L2k,User_L3k,User_L4k,User_L6k,User_L8k FROM Subjects WHERE SubjN == \'%s\'""" % (SubjN))
-        uservar_this = c.fetchone()
-        left = []
-        for var in uservar_this:
-            if is_number(var):
-                left.append(float(var))
-            else:
-                left.append(np.nan)
-        plot_data(af,left,'left')
+            c.execute("""SELECT User_L125,User_L250,User_L500,User_L750,User_L1k,
+                         User_L15,User_L2k,User_L3k,User_L4k,User_L6k,User_L8k FROM Subjects WHERE SubjN == \'%s\'""" % (SubjN))
+            uservar_this = c.fetchone()
+            left = []
+            for var in uservar_this:
+                if var and is_number(var):
+                    left.append(float(var))
+                else:
+                    left.append(np.nan)
+            plot_data(af,left,'left')
 
-        c.execute("""SELECT User_R125,User_R250,User_R500,User_R750,User_R1k,
-                     User_R15,User_R2k,User_R3k,User_R4k,User_R6k,User_R8k FROM Subjects WHERE SubjN == \'%s\'""" % (SubjN))
-        uservar_this = c.fetchone()
-        right = []
-        for var in uservar_this:
-            if is_number(var):
-                right.append(float(var))
-            else:
-                right.append(np.nan)
-        plot_data(af,right,'right')
+            c.execute("""SELECT User_R125,User_R250,User_R500,User_R750,User_R1k,
+                         User_R15,User_R2k,User_R3k,User_R4k,User_R6k,User_R8k FROM Subjects WHERE SubjN == \'%s\'""" % (SubjN))
+            uservar_this = c.fetchone()
+            right = []
+            for var in uservar_this:
+                if var and is_number(var):
+                    right.append(float(var))
+                else:
+                    right.append(np.nan)
+            plot_data(af,right,'right')
 
         fh.show()
 
 if __name__ == "__main__":
-    db = sys.argv[1].strip("\"").strip("\'")
-    SubjN = sys.argv[2]
+    db = None
+    SubjN = None
+    if len(sys.argv) > 1:
+        db = sys.argv[1].strip("\"").strip("\'")
+    if len(sys.argv) > 2:
+        SubjN = sys.argv[2]
     proc_subject(db, SubjN)
 
