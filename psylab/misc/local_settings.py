313b48634ae390185d4b5745f88b1d8f9c3947ad
Christopher Brown <code-breaker@shrsft163.shrs.pitt.edu>
2014-01-23 12:31:54 -0500

    Added 2 functions to misc/local_settings.py.

--- psylab/misc/local_settings.py
+++ psylab/misc/local_settings.py
@@ -52,13 +52,15 @@ class local_settings:
         ...where each section name should consist of os.name ('posix' or 'nt')
         followed by a period, followed by socket.gethostname (machine name)
 
-        There are several functions for getting various types of data. 
+        There are several functions for getting various types of data. But 
+        get_str always works, since that is how all data are stored in the file.
+        Lists of ints or floats are assumed to be comma-separated
         
         Example usage:
         
         import local_settings
         machine = local_settings.local_settings()
-        name = machine.get_string('name')
+        name = machine.get_str('name')
 
     """
     def __init__(self, conf_file=None, machine=None):
@@ -73,6 +75,15 @@ class local_settings:
             self.machine = os.name + "." + socket.gethostname()
         self.Conf = ConfigParser.ConfigParser()
         self.Conf.read(self.conf_file)
+
+    def get_machine(self):
+        return self.machine
+
+    def get_keys(self):
+        keys = []
+        for item in self.Conf.items(self.machine):
+            keys.append(item[0])
+        return keys
 
     def get_str(self, var):
         return self.Conf.get(self.machine, var)
