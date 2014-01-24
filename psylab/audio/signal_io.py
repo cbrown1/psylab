b5e38b11fffdc700d84894289cc86317a06e322f
Christopher Brown <code-breaker@shrsft163.shrs.pitt.edu>
2014-01-24 11:55:47 -0500

    Added get_consecutive_files to audio/signal_io

--- psylab/audio/signal_io.py
+++ psylab/audio/signal_io.py
@@ -29,7 +29,6 @@ def read_multi_file (folder, file_base, file_ext):
         you would pass 'track01' as the file_base parameter, and 
         '.flac' as the file_ext parameter. 
     
-    
     """
     files = glob.glob(os.path.join(folder,file_base+'*'+file_ext))
     files.sort()
@@ -41,4 +40,48 @@ def read_multi_file (folder, file_base, file_ext):
         else:
             y = np.concatenate((y,this_data),axis=1)
     return y
-    
+
+def read_audio_file(file_path):
+    data,fs = m.read_file(file_path)
+    return data,fs
+
+class get_consecutive_files:
+    """A simple class for retrieving files in a directory, one at a time
+
+        Usage:
+        >>> f = get_consecutive_files(path_to_files)
+        >>> f.get_next()
+        'AW001.WAV'
+        >>> f.get_next()
+        'AW002.WAV'
+    """
+    def reset(self):
+        self.index = 0
+        if self.random:
+            np.random.shuffle(self.file_list)
+
+    def __init__(self, path, file_ext='.wav;.WAV',random=False):
+        self.path = path
+        self.file_ext = file_ext.split(';')
+        self.random = random
+        self.file_list = []
+        self.index = 0
+        files = os.listdir( self.path )
+
+        for f in files:
+            fileName, ext = os.path.splitext(f)
+            if not os.path.isdir(f) and ext in self.file_ext:
+                self.file_list.append(f)
+        if self.random:
+            np.random.shuffle(self.file_list)
+        else:
+            self.file_list.sort()
+        self.n = len(self.file_list)
+
+    def get_next(self):
+        item = self.file_list[self.index]
+        self.index += 1
+        if self.index == self.n:
+            self.reset()
+        return item
+
