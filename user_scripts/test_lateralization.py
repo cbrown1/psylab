51064021417b992ddad714103f9d223490e40a57
Christopher Brown <code-breaker@office.fairoaks>
2014-02-09 22:01:15 -0500

    Lateralization form now fully working.

---
+++ user_scripts/test_lateralization.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  9 16:01:24 2014
+
+@author: code-breaker
+"""
+
+from gustav_forms import qt_Lateralization as theForm
+
+feedback = '/home/code-breaker/Projects/psylab/user_scripts/gustav_forms/Images/Animals'
+bg_image = '/home/code-breaker/Projects/psylab/user_scripts/gustav_forms/Images/SmileyFaces/smileyface_headphones.jpg'
+
+
+interface = theForm.Interface(bg_image, feedback)
+
