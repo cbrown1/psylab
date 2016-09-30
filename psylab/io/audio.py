1082b5a1748f82daa1cd213a751111a32c90617a
Christopher Brown <cbrown@cbrown1.psylab.pitt.edu>
2016-09-30 11:04:51 -0400

    Added psylab.io.audio.assign_to_output_channels: Zero-pads columns to assign multi-channel audio data to desired output channels

--- psylab/io/audio.py
+++ psylab/io/audio.py
@@ -17,14 +17,14 @@
 # You should have received a copy of the GNU General Public License
 # along with Psylab.  If not, see <http://www.gnu.org/licenses/>.
 #
-# Bug reports, bug fixes, suggestions, enhancements, or other 
-# contributions are welcome. Go to http://code.google.com/p/psylab/ 
-# for more information and to contribute. Or send an e-mail to: 
+# Bug reports, bug fixes, suggestions, enhancements, or other
+# contributions are welcome. Go to http://code.google.com/p/psylab/
+# for more information and to contribute. Or send an e-mail to:
 # cbrown1@pitt.edu.
 #
 
 """
-Helper functions for reading audio data in less typical ways
+Helper functions for working with audio data in less typical ways
 
 """
 
@@ -34,29 +34,29 @@ import numpy as np
 import medussa as m
 
 def read_multi_file (folder, file_base, file_ext):
-    """Reads audio data split across mulitple files
-        
+    """Reads audio data split across multiple files
+
         A typical use case is audio with more than 8 channels
         saved in flac format, which is limited to 8 channels per
         file. So these data would thus be saved to multiple files.
-        
+
         Function assumes a file format similar to:
-        
+
             track01_1.flac
             track01_2.flac
             track01_3.flac
-        
-        That is, assumes that the file counter is the last part 
-        of the file base name, and that the first file contains 
-        the first set of audio channels, etc.. In the example, 
-        the function will read in all three of these files, and 
-        you would pass 'track01' as the file_base parameter, and 
-        '.flac' as the file_ext parameter. 
-        
+
+        That is, assumes that the file counter is the last part
+        of the file base name, and that the first file contains
+        the first set of audio channels, etc.. In the example,
+        the function will read in all three of these files, and
+        you would pass 'track01' as the file_base parameter, and
+        '.flac' as the file_ext parameter.
+
         Dependencies
         ------------
         Medussa [http://medussa.googlecode.com]
-    
+
     """
     files = glob.glob(os.path.join(folder,file_base+'*'+file_ext))
     files.sort()
@@ -72,3 +72,31 @@ def read_multi_file (folder, file_base, file_ext):
 def read_audio_file(file_path):
     data,fs = m.read_file(file_path)
     return data,fs
+
+def assign_to_output_channels(audio, channels):
+    """Zero-pads columns to assign multi-channel audio data to desired output channels
+
+        Intended for multi-channel playback situations in which non-contiguous
+        output channels are to be used. Columns of zeros will be inserted between channels
+        as needed, so that each channel of audio data will be assigned to the correct output
+        channel upon playback. EG.:
+
+        >>> a = np.ones(4)
+        >>> b = np.ones(4)*2
+        >>> ar = np.column_stack((a,b))
+        >>> out = assign_to_output_channels(ar, [2,6])
+        >>> out
+        array([[ 0.,  0.,  1.,  0.,  0.,  0.,  2.],
+               [ 0.,  0.,  1.,  0.,  0.,  0.,  2.],
+               [ 0.,  0.,  1.,  0.,  0.,  0.,  2.],
+               [ 0.,  0.,  1.,  0.,  0.,  0.,  2.]])
+        >>> 
+    """
+
+
+    i = 0
+    out = np.zeros((audio.shape[0], channels[-1]+1))
+    for channel in channels:
+            out[:,channel] = audio[:,i]
+            i += 1
+    return out
